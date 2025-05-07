7f638bc4225ed63e9dfdb0f02e045ccb6b7d9832
Ryan Posey <ryan.m.posey@gmail.com>
2025-05-07 00:32:17 -0600

    fix + docker

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { watch } = require('fs');
 const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = 'style-loader';
 
